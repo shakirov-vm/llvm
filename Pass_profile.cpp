7762d0811ceb3dc0f444ea27b30f7d903cefa108
Vadim Shakirov <shakirov@ispras.ru>
2023-10-27 22:05:21 +0300

    Add simple all instruction logger

--- Pass_profile.cpp
+++ Pass_profile.cpp
@@ -15,8 +15,7 @@ struct ProfilerPass : public FunctionPass {
   ProfilerPass() : FunctionPass(ID) {}
 
   bool isFuncLogger(StringRef name) {
-    return name == "binOptLogger" || name == "callLogger" ||
-           name == "funcStartLogger" || name == "funcEndLogger" || name == "BranchLogger";
+    return name == "EveryLogger";
   }
 
   virtual bool runOnFunction(Function &F) {
@@ -41,116 +40,38 @@ struct ProfilerPass : public FunctionPass {
     IRBuilder<> builder(Ctx);
     Type *retType = Type::getVoidTy(Ctx);
 
-    // Prepare funcStartLogger function
-    ArrayRef<Type *> funcStartParamTypes = {
-        builder.getInt8Ty()->getPointerTo()};
-    FunctionType *funcStartLogFuncType =
-        FunctionType::get(retType, funcStartParamTypes, false);
-    FunctionCallee funcStartLogFunc = F.getParent()->getOrInsertFunction(
-        "funcStartLogger", funcStartLogFuncType);
-
-    // Insert a call to funcStartLogger function in the function begin
-    BasicBlock &entryBB = F.getEntryBlock();
-    builder.SetInsertPoint(&entryBB.front());
-    Value *funcName = builder.CreateGlobalStringPtr(F.getName());
-    Value *args[] = {funcName};
-    builder.CreateCall(funcStartLogFunc, args);
-
-    // Prepare callLogger function
-    ArrayRef<Type *> callParamTypes = {builder.getInt8Ty()->getPointerTo(),
-                                          builder.getInt8Ty()->getPointerTo(),
-                                          Type::getInt64Ty(Ctx)};
-    FunctionType *callLogFuncType =
-        FunctionType::get(retType, callParamTypes, false);
-    FunctionCallee callLogFunc =
-        F.getParent()->getOrInsertFunction("callLogger", callLogFuncType);
-
-    // Prepare funcEndLogger function
-    ArrayRef<Type *> funcEndParamTypes = {
-        builder.getInt8Ty()->getPointerTo(), Type::getInt64Ty(Ctx)};
-    FunctionType *funcEndLogFuncType =
-        FunctionType::get(retType, funcEndParamTypes, false);
-    FunctionCallee funcEndLogFunc =
-        F.getParent()->getOrInsertFunction("funcEndLogger", funcEndLogFuncType);
-
-    // Prepare binOptLogger function
-    ArrayRef<Type *> binOptParamTypes = {Type::getInt32Ty(Ctx),
-                                            Type::getInt32Ty(Ctx),
-                                            Type::getInt32Ty(Ctx),
-                                            builder.getInt8Ty()->getPointerTo(),
-                                            builder.getInt8Ty()->getPointerTo(),
-                                            Type::getInt64Ty(Ctx)};
-    FunctionType *binOptLogFuncType =
-        FunctionType::get(retType, binOptParamTypes, false);
-    FunctionCallee binOptLogFunc =
-        F.getParent()->getOrInsertFunction("binOptLogger", binOptLogFuncType);
-    
-//int num_suc, int basic, char* braName, char* funcName
-    // Prepare BranchLogger function
-    ArrayRef<Type *> BranchParamTypes = {
-                                            builder.getInt8Ty()->getPointerTo(),
-                                            builder.getInt8Ty()->getPointerTo()};
-    FunctionType *BranchLogFuncType =
-        FunctionType::get(retType, BranchParamTypes, false);
-    FunctionCallee BranchLogFunc =
-        F.getParent()->getOrInsertFunction("BranchLogger", BranchLogFuncType);
+    // Prepare EveryLogger function
+    ArrayRef<Type *> EveryParamTypes = {builder.getInt8Ty()->getPointerTo()};
+    FunctionType *EveryLogFuncType =
+        FunctionType::get(retType, EveryParamTypes, false);
+    FunctionCallee EveryLogFunc =
+        F.getParent()->getOrInsertFunction("EveryLogger", EveryLogFuncType);
 
     // Insert loggers for call, binOpt and ret instructions
     for (auto &B : F) {
       for (auto &I : B) {
         Value *valueAddr =
             ConstantInt::get(builder.getInt64Ty(), (int64_t)(&I));
-        if (auto *call = dyn_cast<CallInst>(&I)) {
-          // Insert before call
-          builder.SetInsertPoint(call);
 
-          // Insert a call to callLogger function
+        if (auto *call = dyn_cast<CallInst>(&I)) {
+
+          // Skip call function
           Function *callee = call->getCalledFunction();
-          if (callee && !isFuncLogger(callee->getName())) {
-            Value *calleeName =
-                builder.CreateGlobalStringPtr(callee->getName());
-            Value *funcName = builder.CreateGlobalStringPtr(F.getName());
-            Value *args[] = {funcName, calleeName, valueAddr};
-            builder.CreateCall(callLogFunc, args);
+          if (callee && isFuncLogger(callee->getName())) {
+            continue;
           }
         }
-        if (auto *ret = dyn_cast<ReturnInst>(&I)) {
-          // Insert before ret
-          builder.SetInsertPoint(ret);
+        if (auto *phi = dyn_cast<PHINode>(&I)) {
+            continue;
+        }    
 
-          // Insert a call to funcEndLogFunc function
-          Value *funcName = builder.CreateGlobalStringPtr(F.getName());
-          Value *args[] = {funcName, valueAddr};
-          builder.CreateCall(funcEndLogFunc, args);
-        }
-        if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-          // Insert after op
-          builder.SetInsertPoint(op);
-          builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+        auto *instr = dyn_cast<Instruction>(&I);
+        // Insert before instruction
+        builder.SetInsertPoint(instr);
 
-          // Insert a call to binOptLogFunc function
-          Value *lhs = op->getOperand(0);
-          Value *rhs = op->getOperand(1);
-          Value *funcName = builder.CreateGlobalStringPtr(F.getName());
-          Value *opName = builder.CreateGlobalStringPtr(op->getOpcodeName());
-          Value *args[] = {op, lhs, rhs, opName, funcName, valueAddr};
-          builder.CreateCall(binOptLogFunc, args);
-        }
-        if (auto *bra = dyn_cast<BranchInst>(&I)) {
-
-//            outs() << "Yes" << "\n";
-
-            builder.SetInsertPoint(bra);
-            
-    //        Value *cond = bra->getCondition();
-            //unsigned num_suc = bra->getNumSuccessors();
-      //      Value *basic = bra->getSuccessor(0);
-
-            Value *funcName = builder.CreateGlobalStringPtr(F.getName());
-            Value *braName = builder.CreateGlobalStringPtr(bra->getOpcodeName());
-            Value *args[] = {braName, funcName};
-            builder.CreateCall(BranchLogFunc, args);
-        }
+        Value *InstrName = builder.CreateGlobalStringPtr(instr->getOpcodeName());
+        Value *args[] = {InstrName};
+        builder.CreateCall(EveryLogFunc, args);
       }
     }
     return true;
